e5d94bd17ef1b3f2d2992c3a4b736092ad8a27b7
OJPARKINSON <olliepar01@gmail.com>
2019-02-02 00:22:06 +0000

    crazy css grid fix

--- src/components/cardImg.js
+++ src/components/cardImg.js
@@ -4,7 +4,7 @@ import '../components/Layout.css'
 const CardImg = props => (
     <div className="Card">
         <img className="leftCard" alt={props.alternative}  src={props.image}></img>
-        <a class="igtag" href={props.credits}><p>{props.igtag}</p></a>
+        <a target="_blank" class="igtag" href={props.credits}><p>{props.igtag}</p></a>
     </div>
 )
 
