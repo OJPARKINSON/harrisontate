bb6b6260cbe0ec8b1d85c194bf435979427ae8e8
OJPARKINSON <olliepar01@gmail.com>
2022-06-01 23:29:48 +0100

    Made a start

---
+++ lib/utils.ts
@@ -0,0 +1,19 @@
+export async function fetchGraphQL(query: string, preview = false) {
+  const token = preview ? process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN : process.env.CONTENTFUL_ACCESS_TOKEN
+  const entries = await fetch(
+    `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({ query }),
+    }
+  ).then((response) => response.json())
+  return extractPostEntries(entries)
+}
+
+function extractPostEntries(fetchResponse: any) {
+  return fetchResponse?.data?.postCollection?.items
+}
