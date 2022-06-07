88ba54bcdf9b8ee51242e86150a4a3578613a22b
Oliver Parkinson <olliepar01@gmail.com>
2022-06-08 00:26:59 +0100

    added lint

--- components/layout.tsx
+++ components/layout.tsx
@@ -9,28 +9,21 @@ interface LayoutProps {
   children: React.ReactNode
 }
 
-const Layout: React.FC<LayoutProps> = ({
-  title,
-  styling,
-  children,
-  whiteFooter,
-}) => {
-  return (
-    <div className="container">
-      <Head>
-        <title>Harrison Tate | {title}</title>
-      </Head>
-      <Header styling={styling || ''} />
-      <main>{children}</main>
-      <footer style={{ color: whiteFooter ? 'white' : 'black' }}>
-        <h3>
-          © {new Date().getFullYear()}, Built by
-          {` `}
-          <a href="http://oliverparkinson.co.uk">Oliver Parkinson</a>
-        </h3>
-      </footer>
-    </div>
-  )
-}
+const Layout = ({ title, styling, children, whiteFooter }: LayoutProps) => (
+  <div className="container">
+    <Head>
+      <title>Harrison Tate | {title}</title>
+    </Head>
+    <Header styling={styling || ''} />
+    <main>{children}</main>
+    <footer style={{ color: whiteFooter ? 'white' : 'black' }}>
+      <h3>
+        © {new Date().getFullYear()}, Built by
+        {` `}
+        <a href="http://oliverparkinson.co.uk">Oliver Parkinson</a>
+      </h3>
+    </footer>
+  </div>
+)
 
 export default Layout
