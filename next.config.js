da76515d85195d30bf17c15478f11b26e9c2aa0c
OJPARKINSON <olliepar01@gmail.com>
2025-05-11 19:09:39 +0100

    fixed images

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['images.ctfassets.net'],
-    deviceSizes: [350, 450, 550, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.ctfassets.net',
+        pathname: '**',
+      },
+    ],
   },
 }
